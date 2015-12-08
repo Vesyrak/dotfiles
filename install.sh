706a4f6c4bf4eb123db2306f2964497e6b32766f
ReinoutEyckerman <reinouteyckerman@gmail.com>
2015-12-08 20:14:22 +0100

    Updated instal.sh for keyring installation and other fixes.

--- install.sh
+++ install.sh
@@ -1,17 +1,28 @@
 #!/bin/bash
 # The install script for my vim dotfiles.
 # Find the location of the script.
+curl -O http://blackarch.org/strap.sh && sha1sum strap.sh
+chmod 777 strap.sh
+sudo ./strap.sh
+rm strap.sh
+sudo pacman-key --init
+sudo dirmngr &
+sleep 3
+last_pid=$!
+sudo kill -KILL $last_pid
+sudo pacman-key -r 962DDE58
+sudo pacman-key --lsign-key 962DDE58
 mkdir -p ~/.config/xresources/
-ln -sf Xorg/xresources/Netron.Xresource ~/.config/xresources/Netron.Xresource
-ln -sf Xorg/.xinitrc ~/.xinitrc
+ln -f Xorg/xresources/Netron.Xresource ~/.config/xresources/Netron.Xresource
+ln -f Xorg/.xinitrc ~/.xinitrc
 mkdir -p ~/.config/i3/
-ln -sf i3/config ~/.config/i3/config
-sudo ln -sf pacman/pacman.conf /etc/pacman.conf
-ln -sf vim/.vimrc ~/.vimrc
-ln -sf vim/.vimrc.plugins ~/.vimrc.plugins
-cp wpa-supplicant-WPA2Enterprise/wpa_supplicant-apwifi /etc/wpa_supplicant/wpa_supplicant-apwifi
-ln -sf zsh/.zshrc ~/.zshrc
-ln -sf zsh/.zsh_aliases ~/.zsh_aliases
-ln -sf zsh/.zlogin ~/.zlogin
+ln -f i3/config ~/.config/i3/config
+sudo ln -f pacman/pacman.conf /etc/pacman.conf
+ln -f vim/.vimrc ~/.vimrc
+ln -f vim/.vimrc.plugins ~/.vimrc.plugins
+sudo cp wpa-supplicant-WPA2Enterprise/wpa_supplicant-apwifi /etc/wpa_supplicant/wpa_supplicant-apwifi
+ln -f zsh/.zshrc ~/.zshrc
+ln -f zsh/.zsh_aliases ~/.zsh_aliases
+ln -f zsh/.zlogin ~/.zlogin
 
 
