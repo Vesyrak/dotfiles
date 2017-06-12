1c5d957b4dbb70c7b1d3ea74328ca050f661981a
reinouteyckerman <reinouteyckerman@gmail.com>
2017-06-12 23:03:32 +0200

    Install now installs all at once, minimised required input in install script. Added oepvpn through external script

--- install.sh
+++ install.sh
@@ -2,15 +2,19 @@
 
 user=false
 CONFIG_FILE="$HOME/.config/sysconf/dotfiles.cfg"
+INSTALL_QUEUE=""
+CONFIG_QUEUE=""
 
-function checkconf(){
+function checkconf()
+{
     echo ":: Reading configuration"
     if !([ -f $CONFIG_FILE ]); then
         echo ":: No config file found. Make sure you run a regular install with this script."
     fi
 }
 
-function confenable(){
+function confenable()
+{
     if !([ -f $CONFIG_FILE ]); then
         mkdir -p $HOME/.config/sysconf/
         touch $CONFIG_FILE
@@ -20,7 +24,8 @@ function confenable(){
     fi
 }
 
-function checksudo(){
+function checksudo()
+{
     if !(which sudo > /dev/null); then
         echo ":: Warning: sudo has to be installed before running this script."
         echo ":: Exiting"
@@ -28,15 +33,9 @@ function checksudo(){
     fi
 }
 
-function checkstow(){
-    if !(which stow > /dev/null); then
-        sudo pacman -S stow
-    fi
-}
-
 function createuser()
 {
-    read -p ":: Do you want to create a new user? [Y/n] " -n 1 -r
+    read -p ":: Do you want to create a new user? [Y/n] " -r
     if [[ $REPLY =~ ^[Nn]$ ]]
     then
         exit 1
@@ -64,48 +63,57 @@ function checkuser()
     fi
 }
 
-function mainmin(){
-    echo ":: Starting System Update"
-    sudo pacman -Syu
-    sudo pacman -S --needed - < minpkglist
-    askaur
-    echo ":: System Update Finished"
-    ssh
-}
-
 function main()
 {
-    echo ":: Starting System Update"
-    sudo pacman -Syu
-    sudo pacman -S --needed - < minpkglist
-    sudo pacman -S --needed - < majpkglist
-    askaur
-    echo ":: System Update Finished"
     echo ":: Enabling NetworkManager"
     sudo systemctl start NetworkManager
     sudo systemctl enable NetworkManager
-    lightdm
-    echo ":: Starting AUR Installs"
-    pacaur -Syu
-    pacaur -S an2linuxserver-git
     echo ":: Enabling An2Linux server"
     an2linuxserver.py
     systemctl --user enable an2linuxserver.service
     echo ":: Don't forget to manually pair your device first, by running an2linuxserver.py"
-    echo ":: AUR Update Finished"
     echo ":: Updating File Locations"
     sudo updatedb
-    echo ":: File Locations Updated"
 }
 
-function lightdm(){
-    sudo pacman -S --needed lightdm lightdm-gtk-greeter
-    pacaur -S gtk-theme-arc-git
-    echo ":: Enabling lightdm"
+function askaur()
+{
+    if !(which pacaur > /dev/null); then
+        read -p $'\x0a:: Do you want to install an AUR helper? [Y/n]' -r
+        if [[ $REPLY =~ ^[Yy]$ ]] || [[ $REPLY == "" ]];then
+            aur
+        else
+            echo $'\x0a:: Pacaur is required for this script to function properly.'
+            echo ":: Exiting"
+            exit 0
+        fi
+    fi
+}
+
+function install()
+{
+    echo ":: Starting System Update"
+    askaur
+    pacaur -S --needed  $INSTALL_QUEUE
+    echo ":: System Update Finished"
+}
+
+function configure(){
+    echo ":: Starting Configuring"
+    arr=($CONFIG_QUEUE)
+    for config in ${arr[@]}; do
+        echo "$config"
+        eval "$config"
+    done
+    echo ":: Configuring Finished"
+}
+
+function lightdm()
+{
+    echo ":: Configuring Lightdm"
     sudo systemctl enable lightdm
-    echo ":: Stowing lightdm config"
     sudo ln -sf $PWD/lightdm/etc/lightdm/* /etc/lightdm/
-    echo ":: Granting lightdm access to config file"
+    confenable lightdm 2
     setfacl -m u:lightdm:rx ~/
     setfacl -m u:lightdm:rx ~/repos
     setfacl -m u:lightdm:rx $PWD
@@ -114,7 +122,6 @@ function lightdm(){
     echo ":: Setting VNC passwd"
     sudo vncpasswd /etc/vncpasswd
     echo ":: Don't forget to install a wallpaper for lightdm/awesome, otherwise ERRORS ENSURED"
-    confenable lightdm 2
 }
 
 function ssh()
@@ -130,7 +137,7 @@ function ssh()
 function aur()
 {
     echo ":: Installing AUR"
-    sudo pacman -S cower expac
+    sudo pacman -Syu
     wget https://aur.archlinux.org/cgit/aur.git/snapshot/pacaur.tar.gz
     tar -xvf pacaur.tar.gz
     cd pacaur
@@ -143,49 +150,36 @@ function aur()
 
 function zsh()
 {
-    checkstow
-    echo ":: Installing ZSH"
-    sudo pacman -S --needed zsh zsh-completions pkgfile
-    sudo pkgfile --update
-    pacaur -S oh-my-zsh-git
-    echo ":: ZSH Installation Finished. Changing Shell..."
-    chsh -s /bin/zsh
-    echo ":: Default Shell is now ZSH"
-    echo ":: Reload Shell to see effects"
-    echo ":: Installing ZSH config"
+    echo ":: Configuring ZSH"
     stow -t ~/ zsh
     confenable zsh 0
+    sudo pkgfile --update
+    echo ":: Changing Shell"
+    chsh -s /bin/zsh
+    echo ":: Reload Shell to see effects"
 }
 
 function xonsh()
 {
-    checkstow
-    echo ":: Installing XONSH"
-    pacaur -S xonsh
-    echo ":: XONSH Installation Finished. Changing Shell.."
-    chsh -s /usr/bin/xonsh
-    echo ":: Default Shell is now XONSH"
-    echo ":: Reload Shell to see effects"
-    echo ":: Installin XONSH $ ZSH config used by XONSH"
+    echo ":: Configuring XONSH"
     stow -t ~/ zsh
-    stow -t ~/ xonsh
     confenable zsh 0
+    stow -t ~/ xonsh
     confenable xonsh 0
+    echo ":: Changing Shell.."
+    chsh -s /usr/bin/xonsh
+    echo ":: Reload Shell to see effects"
 }
 
 function delugeserver()
 {
-    checkstow
-    echo ":: Setting up Deluge Server"
-    sudo pacman -S --needed deluge
+    echo ":: Configuring Deluge Server"
     sudo systemctl enable deluged
     sudo systemctl start deluged
-    echo ":: Deluge Server Finished"
-    echo ":: Setting up Deluge WebServer"
-    sudo pacman -S --needed python2-service-identity python2-mako
+    sudo sed -i "s/\(User*=*\).*/\1$USER/" /etc/systemd/system/deluged.service
+    echo ":: Configuring Deluge WebServer"
     stow -t ~/ deluge
     sudo cp /usr/lib/systemd/system/deluged.service /etc/systemd/system/deluged.service #TODO Check if symlink works
-    sudo sed -i "s/\(User*=*\).*/\1$USER/" /etc/systemd/system/deluged.service
     echo ":: Creating deluge auth file"
     read -p ":: Please enter the desired username: " name
     read -p ":: Please enter the desired password: " passwd
@@ -196,57 +190,40 @@ function delugeserver()
 
 function bluetooth()
 {
-    echo ":: Installing Bluetooth"
-    sudo pacman -S --needed blueman
+    echo ":: Configuring Bluetooth"
     sudo systemctl start bluetooth
     sudo systemctl enable bluetooth
-    echo ":: Finished Installing Bluetooth"
 }
 
 function awesome()
 {
-    checkstow
-    echo ":: Installing awesome"
-    sudo pacman -S --needed awesome
-    pacaur -S lain-git
-    echo ":: Installation Finished"
-    echo ":: Configuring..."
+    echo ":: Configuring AwesomeWM"
     stow -t ~/ awesome
-    echo ":: Finished Installing AwesomeWM"
     confenable awesome 0
 }
 
 function i3()
 {
-    checkstow
-    echo ":: Installing i3"
-    pacaur -S i3status i3lock-blur i3-gaps rofi compton py3status python-mpd2 python-requests
+    echo ":: Configuring i3"
     stow -t ~/ i3
-    echo ":: Finished Installing i3WM"
-    echo ":: Installing Compton"
-    sudo pacman -S --needed compton
-    stow -t ~/ compton
-    echo ":: Finished Installing Compton"
-    echo":: Installing dunst"
-    sudo pacman -S --needed dunst
-    stow -t ~/ dunst
-    echo ":: Finished Installing dunst"
     confenable i3 0
+    echo ":: Installing Compton"
+    stow -t ~/ compton
     confenable compton 0
+    echo":: Installing dunst"
+    stow -t ~/ dunst
     confenable dunst 0
 }
 
 function config()
 {
-    checkstow
     echo ":: Installing Config Files"
     echo ":: Installing XResources"
     mkdir -p ~/.config/xresources/
     stow -t ~/ Xresources
+    confenable Xresources 0
     echo ":: Installing Vim"
     stow -t ~/ vim
-    echo ":: Finished Installing Config Files"
-    confenable Xresources 0
     confenable vim 0
 }
 
@@ -296,12 +273,31 @@ function audioclient()
     echo ":: Updated ssh config file for easy server access under 'mpd'"
 }
 
-function automountServer()
+function installvpn()
 {
-    echo ":: Putting media drive in automount. If you ever change harddrive, change the UUID"
-    echo "UUID=4f7a9f5a-2bf7-46e2-9560-29f0326293e4 /media ext4 acl,noatime,nofail,x-systemd.device-timeout=10 0 2" >> /etc/fstab
-}
+   #https://github.com/Angristan/OpenVPN-install
+    echo ":: Installing openvpn through git script"
+    mkdir $PWD/tmp
+    cd tmp
+    git clone https://github.com/Angristan/OpenVPN-install.git
+    cd OpenVPN-install
+    chmod +x openvpn-install.sh
+    sudo ./openvpn-install.sh
+    cd ../../
+    rm tmp -r
+    echo ":: Due to current bugs in the script for the Arch distribution, several files still have to be moved/generated."
+    echo ":: Please check once in a while if this is still necessary."
+    sudo mv /etc/openvpn/server.conf /etc/openvpn/server/
+    sudo mv /etc/openvpn/ca.crt /etc/openvpn/server/
+    sudo mv /etc/openvpn/server.key /etc/openvpn/server/
+    sudo mv /etc/openvpn/server.crt /etc/openvpn/server/
+    sudo mv /etc/openvpn/crl.pem /etc/openvpn/server/
+    sudo mv /etc/openvpn/tls-auth.key /etc/openvpn/server/
+    sudo openssl dhparam -out /etc/openvpn/server/dh.pem 2048
+    systemctl enable openvpn-server@server
+    systemctl start openvpn-server@server
 
+}
 #TODO Review
 function piholeclient()
 {
@@ -326,84 +322,70 @@ function piholeclient()
 function audioserver()
 {
     sudo mkdir /media
-    read -p ":: If nessecary, is your HDD already mounted on /media? [Y/n]" -n -r
+    read -p ":: If nessecary, is your HDD already mounted on /media? [Y/n]" -r
     if [[ $REPLY =~ ^[Nn]$ ]]; then
         read -p ":: Please enter device name (e.g. /dev/sda2)"
         echo ":: Mounting $REPLY"
         sudo mount $REPLY /media
+        read -p $'\x0a:: I assume you also want the drive auto-mounted? [Y/n]' -r
+        if [[ $REPLY =~ ^[Yy]$ ]] || [[ $REPLY == "" ]];then
+            echo ":: Putting $REPLY drive in automount."
+            UUID=$(sudo blkid /dev/sda1)
+            echo "UUID=$UUID /media ext4 acl,noatime,nofail,x-systemd.device-timeout=10 0 2" >> /etc/fstab
+        fi
     fi
-    checkstow
-    sudo pacman -S --needed mpd mlocate screenfetch alsa-utils
+    echo ":: Configuring mpd"
     stow -t ~/ mpd
+    confenable mpd 0
     setfacl -m "u:mpd:rwx" /media
-    # mpd bootin'
-    mpd
-    sleep 1
-    sudo systemctl stop mpd
     sudo sed  '/\[Unit\]/a RequiresMountsFor=/media/' /usr/lib/systemd/user/mpd.service
     systemctl --user enable mpd
     systemctl --user start mpd
     loginctl enable-linger $USER
-    echo ":: This uses PulseAudio. Installing..."
-    sudo pacman -S --needed pulseaudio
+    echo ":: Installing PulseAudio Service."
     sudo stow -t / pulseService
     sudo systemctl --user enable pulseaudio #todo unsynced with transceiver
     sudo systemctl --user start pulseaudio
-    # Makes sure the wifi-dongle doesn't power off causing connection issues
-    sudo stow -t / WLanPOFix
-    echo ":: Installing Beets audio manager"
-    sudo pacman -S --needed beets
-    stow -t ~/ beets
-    echo ":: Installing Beets extension dependencies"
-    pacaur -S python2-discogs-client
-    sudo pacman -S --needed python2-flask
-    echo ":: Beets installed"
-    echo ":: Installing Audio Client"
-    sudo pacman -S --needed ncmpcpp
-    stow -t ~/ ncmpcpp
-    echo ":: Finished Installing Audio Client"
-    confenable mpd 0
     confenable pulseService 1
+    # Makes sure the wifi-dongle doesn't power off causing connection issues
+    echo ":: Making sure the Wi-Fi connection doesn't sleep"
+    sudo stow -t / WLanPOFix
     confenable WLanPOFix 1
+    echo ":: Configuring Beets audio manager"
+    stow -t ~/ beets
     confenable beets 0
+    echo ":: Configuring Audio Client"
+    stow -t ~/ ncmpcpp
     confenable ncmpcpp 0
-
 }
 
 function pulsetransceiver()
 {
-    checkstow
-    sudo pacman -S --needed pulseaudio-zeroconf avahi paprefs pavucontrol ttf-droid
+    echo ":: Configuring PulseAudio for streaming"
+    stow -t ~/ pulsetransceiver
+    confenable pulsetransceiver 0
     sudo systemctl start avahi-daemon
     sudo systemctl enable avahi-daemon
-    stow -t ~/ pulsetransceiver
     systemctl --user enable pulseaudio
     loginctl enable-linger $USER
-    read -p ":: Do you also want Bluetooth streaming on this device? [Y/n]" -n 1 -r
-    if [[ $REPLY =~ ^[Yy]$ ]] || [[ $REPLY == "" ]];then
-        pulsebluetooth
-    fi
-    confenable pulsetransceiver 0
-
 }
 
 function pulsebluetooth()
 {
-    sudo pacman -S --needed pulseaudio-alsa pulseaudio-bluetooth bluez bluez-libs bluez-utils bluez-firmware
+    echo ":: Configuring PulseAudio for Bluetooth"
     sudo ln -sf $PWD/pulseBluetooth/etc/bluetooth/* /etc/bluetooth/
     echo ":: You still need to manually pair and trust the device fam"
     confenable pulseBluetooth 2
 }
 
 function windowspassthrough(){
-    sudo pacman -S --needed virt-manager qemu libvirt ovmf bridge-utils
+    echo ":: Enabling libvirt for passthrough"
     sudo systemctl enable --now libvirtd
     sudo systemctl enable virtlogd.socket
     echo ":: Read the README in ./vm for instructions how to do the passthrough"
 }
 
 function restow(){
-    checkstow
     checkconf
     while read config sudo; do
         echo $config
@@ -425,16 +407,6 @@ function restow(){
                 ;;
         esac
     done < $CONFIG_FILE
-
-}
-
-function askaur(){
-    read -p $'\x0a:: Do you want to install an AUR helper? [Y/n]' -n 1 -r
-    if [[ $REPLY =~ ^[Yy]$ ]] || [[ $REPLY == "" ]];then
-        aur
-    else
-        echo ":: Sure thing, ignore the potential errors coming up though. I'll fix this later."
-    fi
 }
 
 checkuser
@@ -449,83 +421,103 @@ for i in "$@"; do
             ;;
     esac
 done
-read -p $'\x0a:: Do you want to install a minimal package list? [Y/n]' -n 1 -r
+read -p ':: Do you want to install a minimal package list? [Y/n]' -r
 if [[ $REPLY =~ ^[Yy]$ ]] || [[ $REPLY == "" ]];then
-    mainmin
+    INSTALL_QUEUE+=$(cat minpkglist)
 else
-    read -p $'\x0a:: Do you want to install a complete package list? [Y/n]' -n 1 -r
+    read -p ':: Do you want to install a complete package list? [Y/n]' -r
     if [[ $REPLY =~ ^[Yy]$ ]] || [[ $REPLY == "" ]];then
-        main
+        INSTALL_QUEUE+=$(cat minpkglist)
+        INSTALL_QUEUE+=$(cat majpkglist)
+        INSTALL_QUEUE+=" "
+        INSTALL_QUEUE+="an2linuxserver-git lightdm lightdm-gtk-greeter gtk-theme-arc-git "
+        CONFIG_QUEUE+="main "
     fi
 fi
-read -p $'\x0a:: Do you want to use this machine as a Deluge Server? [Y/n]' -n 1 -r
+read -p ':: Do you want to use this machine as a Deluge Server? [Y/n]' -r
 if [[ $REPLY =~ ^[Yy]$ ]] || [[ $REPLY == "" ]];then
-    delugeserver
+    INSTALL_QUEUE+="python2-service-identity python2-mako deluge "
+    CONFIG_QUEUE+="delugeserver "
 fi
-read -p $'\x0a:: Do you want to use this machine as a tt-rss server? [Y/n]' -n 1 -r
+read -p ':: Do you want to use this machine as a tt-rss server? [Y/n]' -r
 if [[ $REPLY =~ ^[Yy]$ ]] || [[ $REPLY == "" ]];then
-    tt-rss
+    #Ayy lmao#TODO
+    CONFIG_QUEUE+="tt-rss "
 fi
-read -p $'\x0a:: Do you want to install/update your configuration files? [Y/n]' -n 1 -r
+read -p ':: Do you want to install/update your configuration files? [Y/n]' -r
 if [[ $REPLY =~ ^[Yy]$ ]] || [[ $REPLY == "" ]];then
-    config
+    CONFIG_QUEUE+="config "
 fi
-read -p $'\x0a:: Do you want to install the BlackArch repositories? [Y/n]' -n 1 -r
+read -p ':: Do you want to install the BlackArch repositories? [Y/n]' -r
 if [[ $REPLY =~ ^[Yy]$ ]] || [[ $REPLY == "" ]];then
-    blackarch
+    CONFIG_QUEUE+="blackarch "
 fi
-read -p $'\x0a:: Do you want to use this machine for gaming? [Y/n]' -n 1 -r
+read -p ':: Do you want to use this machine for passthrough? [Y/n]' -r
 if [[ $REPLY =~ ^[Yy]$ ]] || [[ $REPLY == "" ]];then
-    gaming
+    INSTALL_QUEUE+="virt-manager qemu libvirt ovmf bridge-utils "
+    CONFIG_QUEUE+="windowspassthrough "
 fi
-read -p $'\x0a:: Do you want to use this machine as an Audio Server? [Y/n]' -n 1 -r
+read -p ':: Do you want to use this machine as an Audio Server? [Y/n]' -r
 if [[ $REPLY =~ ^[Yy]$ ]] || [[ $REPLY == "" ]];then
-    audioserver
-    read -p $'\x0a:: I assume you also want the drive auto-mounted? [Y/n]' -n 1 -r
-
-    if [[ $REPLY =~ ^[Yy]$ ]] || [[ $REPLY == "" ]];then
-        automountServer
-    fi
+    INSTALL_QUEUE+="mpd mlocate screenfetch alsa-utils pulseaudio beets python2-discogs-client python2-flask ncmpcpp "
+    CONFIG_QUEUE+="audioserver "
 else
-    read -p $'\x0a:: Do you want to use this machine as an Audio Client instead? [Y/n]' -n 1 -r
+    read -p ':: Do you want to use this machine as an Audio Client instead? [Y/n]'  -r
     if [[ $REPLY =~ ^[Yy]$ ]] || [[ $REPLY == "" ]];then
-        audioclient
+        CONFIG_QUEUE+="audioclient "
     fi
 fi
-read -p $'\x0a:: Do you want Bluetooth? [Y/n]' -n 1 -r
+read -p ':: Do you want Bluetooth? [Y/n]' -r
 if [[ $REPLY =~ ^[Yy]$ ]] || [[ $REPLY == "" ]];then
-    bluetooth
+    INSTALL_QUEUE+="blueman "
+    CONFIG_QUEUE+="bluetooth "
 fi
-read -p $'\x0a:: Should this device be able to stream audio to/from other devices? [Y/n]' -n 1 -r
+read -p ':: Should this device be able to stream audio to/from other devices? [Y/n]'  -r
 if [[ $REPLY =~ ^[Yy]$ ]] || [[ $REPLY == "" ]];then
-    pulsetransceiver
+    INSTALL_QUEUE+="pulseaudio-zeroconf avahi paprefs pavucontrol ttf-droid "
+    CONFIG_QUEUE+="pulsetransceiver "
+    read -p ":: Do you also want Bluetooth streaming on this device? [Y/n]" -r
+    if [[ $REPLY =~ ^[Yy]$ ]] || [[ $REPLY == "" ]];then
+        INSTALL_QUEUE+="pulseaudio-alsa pulseaudio-bluetooth bluez bluez-libs bluez-utils bluez-firmware "
+        CONFIG_QUEUE+="pulsebluetooth "
+    fi
 fi
-read -p $'\x0a:: Will this machine be connected to a pi-hole? [Y/n]' -n 1 -r
+read -p ':: Will this machine be connected to a pi-hole? [Y/n]' -r
 if [[ $REPLY =~ ^[Yy]$ ]] || [[ $REPLY == "" ]];then
-    piholeclient
+    CONFIG_QUEUE+="piholeclient "
 fi
-read -p $'\x0a:: Will this machine use awesome as WM? [Y/n]' -n 1 -r
+read -p ':: Will this machine use awesome as WM? [Y/n]' -r
 if [[ $REPLY =~ ^[Yy]$ ]] || [[ $REPLY == "" ]];then
-    awesome
+    INSTALL_QUEUE+="awesome lain-git "
+    CONFIG_QUEUE+="awesome "
 else
-    read -p $'\x0a:: Will this machine use i3 instead? [Y/n]' -n 1 -r
+    read -p ':: Will this machine use i3 instead? [Y/n]' -r
     if [[ $REPLY =~ ^[Yy]$ ]] || [[ $REPLY == "" ]];then
-        i3
+        INSTALL_QUEUE+="i3lock-blur i3-gaps rofi compton py3status python-mpd2 python-requests compton dunst "
+        CONFIG_QUEUE+="i3 "
     fi
 fi
-read -p $'\x0a:: Do you want to install zsh as shell? [Y/n]' -n 1 -r
+read -p ':: Do you want to install zsh as shell? [Y/n]' -r
 if [[ $REPLY =~ ^[Yy]$ ]] || [[ $REPLY == "" ]];then
-    zsh
+    INSTALL_QUEUE+="oh-my-zsh-git zsh zsh-completions pkgfile "
+    CONFIG_QUEUE+="zsh "
 else
-    echo $'\x0a:: WARNING: THE FOLLOWING IS STILL VERY EXPERIMENTAL'
-    read -p $'\x0a:: Do you want to install xonsh as shell instead? [Y/n]' -n 1 -r
+    echo ':: WARNING: THE FOLLOWING IS STILL VERY EXPERIMENTAL'
+    read -p ':: Do you want to install xonsh as shell instead? [Y/n]' -r
     if [[ $REPLY =~ ^[Yy]$ ]] || [[ $REPLY == "" ]];then
-        xonsh
+        INSTALL_QUEUE+="xonsh "
+        CONFIG_QUEUE+="xonsh "
     fi
 fi
-read -p $'\x0a:: Is this an odroid C2 without working audio? [Y/n]' -n 1 -r
+read -p ':: Is this an odroid C2 without working audio? [Y/n]' -r
 if [[ $REPLY =~ ^[Yy]$ ]] || [[ $REPLY == "" ]];then
-    odroidC2audiofix
+    CONFIG_QUEUE+="OdroidC2AudioFix "
 fi
-echo $'\x0a:: Install script terminating'
+read -p ':: Set up as VPN point? [Y/n]' -r
+if [[ $REPLY =~ ^[Yy]$ ]] || [[ $REPLY == "" ]];then
+    CONFIG_QUEUE+="installvpn "
+fi
+install
+configure
+echo ':: Install script terminating'
 
