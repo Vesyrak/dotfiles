ed2fbce7b33a2fbc9687f108d10214c9796b73a9
Vesyrak <reinouteyckerman@gmail.com>
2019-06-09 14:21:00 +0200

    Updated i3 install to i3-gaps

--- scripts/ki3.sh
+++ scripts/ki3.sh
@@ -3,7 +3,19 @@ for f in ./scripts/core/*; do source $f; done
 
 function i3kubuntu()
 {
-  install i3
+  install -y libxcb1-dev libxcb-keysyms1-dev libpango1.0-dev libxcb-util0-dev libxcb-icccm4-dev libyajl-dev libstartup-notification0-dev libxcb-randr0-dev libev-dev libxcb-cursor-dev libxcb-xinerama0-dev libxcb-xkb-dev libxkbcommon-dev libxkbcommon-x11-dev autoconf libxcb-xrm0 libxcb-xrm-dev automake
+  
+  cd /tmp
+  git clone https://www.github.com/Airblader/i3 i3-gaps
+  cd i3-gaps
+
+  autoreconf --force --install
+  rm -rf build/
+  mkdir -p build && cd build/
+
+  ../configure --prefix=/usr --sysconfdir=/etc --disable-sanitizers
+  make
+  sudo make install
   print "Configuring i3"
   stow -t ~/ i3
   confenable i3 0
