dea7a419438b815198e59d6fec93bd002cac1c48
Reinout Eyckerman <reinout@gorilla.co>
2023-08-15 14:53:14 +0200

    feat(nvim): enabled global clipboard, changed color scheme to edge

--- .config/nvim/lua/vars.lua
+++ .config/nvim/lua/vars.lua
@@ -2,15 +2,18 @@
 
 local g = vim.g
 
--- NVim Tree
---g.loaded_netrw = 1
---g.loaded_netrwPlugin = 1
+-- Copy to clipboard
+vim.cmd("set clipboard=unnamedplus")
 
 -- Colors
 g.t_co = 256
 g.background = "dark"
 -- Blamer
 g.blamer_delay = 300
+
+-- Color-scheme
+g.edge_style = "aura"
+g.edge_better_performance = 1
 
 -- Put /site in package path
 local packer_path = vim.fn.stdpath("config") .. "/site"
@@ -23,14 +26,14 @@ g["test#python#pytest#options"] = "--disable-warnings"
 local md_augroup = vim.api.nvim_create_augroup("Markdown Settings", { clear = true })
 
 vim.api.nvim_create_autocmd("BufWritePre", {
-    pattern = { "*.md" },
-    group = md_augroup,
-    command = "setlocal textwidth=80 wrap",
+	pattern = { "*.md" },
+	group = md_augroup,
+	command = "setlocal textwidth=80 wrap",
 })
 
 local python_augroup = vim.api.nvim_create_augroup("Python Settings", { clear = true })
 vim.api.nvim_create_autocmd("BufNewFile,BufRead", {
-    pattern = { "*.py" },
-    group = md_augroup,
-    command = "setlocal foldmethod=indent",
+	pattern = { "*.py" },
+	group = md_augroup,
+	command = "setlocal foldmethod=indent",
 })
