ca2a7ca96761fdfab595a9140a947cf703054d0c
Reinout Eyckerman <reinout@gorilla.co>
2024-06-01 10:45:52 +0200

    feat(zsh): better logging script

--- .dotfiles/scripts/addnote.sh
+++ .dotfiles/scripts/addnote.sh
@@ -2,32 +2,58 @@ date=$(date +%Y-%m-%d);
 time=$(date +%T);
 base_dir="${HOME}/notes/weekly_notes"
 
-week=$(date +%V);
+week=$(date +%V-%Y);
 file="$base_dir/week-$week.md"
-last_week=$(date -v-1w +%V)
+last_week=$(date -v-1w +%V-%Y)
 last_week_file="week-$last_week.md"
-next_week=$(date -v+1w +%V)
+next_week=$(date -v+1w +%V-%Y)
 next_week_file="week-$next_week.md"
 
-other_args=""
+content=""
 list_type="-"
-for arg in "$@"
-do
-    case $arg in
+while [[ $# -gt 0 ]]; do
+    case $1 in
         --task)
-            list_type="[ ] "
+            list_type="[ ]"
+            time=$date
+            shift
+            ;;
+        --header)
+            header="$2"
+            shift
+            shift
             ;;
         *)
-            other_args+="$arg "
+            content+="$1 "
+            shift
             ;;
     esac
 done
 
-default="# Week $week\n Last week: [[$last_week_file]], Next week: [[$next_week_file]]"
 
-if [ ! -f "$file" ]; then
-    echo "$default" > "$file"
+if [ ! -f $file ]; then
+    default="# Week $week\nLast week: [[$last_week_file]], Next week: [[$next_week_file]]\n\n"
+
+    default="${default}## $(date -vmon +%a\ %Y-%m-%d)\n\n"
+    default="${default}## $(date -vtue +%a\ %Y-%m-%d)\n\n"
+    default="${default}## $(date -vwed +%a\ %Y-%m-%d)\n\n"
+    default="${default}## $(date -vthu +%a\ %Y-%m-%d)\n\n"
+    default="${default}## $(date -vfri +%a\ %Y-%m-%d)\n\n"
+    default="${default}## $(date -vsat +%a\ %Y-%m-%d)\n\n"
+    default="${default}## $(date -vsun +%a\ %Y-%m-%d)\n\n"
+
+    default="${default}# TODO\n\n"
+    default="${default}## Previous TODOs\n\n"
+    previous_todos=$(grep -N "^\s*\[ \]" ~/notes/weekly_notes/week-13-2024.md)  
+    default="${default}${previous_todos}\n\n"
+
+    default="${default}# Improvements\n\n"
+
+    echo $default > $file
+
+
 fi
 
+
 grep -q "$header" $file || echo "\n$header" >> $file;
-sed -i "" "s/$header/&\n$list_type ${time}: $other_args/" $file; 
+gawk -i inplace "/$header$/ {print; print \"$list_type $time: $content\"; next} 1" $file
