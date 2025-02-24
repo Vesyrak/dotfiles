dda2419f818f9e2e733e6a2a1ac24f1f7e07e481
Reinout Eyckerman <reinout@gorilla.co>
2025-02-24 22:12:59 +0100

    feat(nvim): filesize-based plugin loading

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -3,13 +3,15 @@
 -- LEADER
 -- These keybindings need to be defined before the first /
 -- is called; otherwise, it will default to "\"
+
 vim.g.mapleader = " "
 
 vim.g.localleader = "\\"
 
--- IMPORTS
-require("vars") -- Variables
-require("opts") -- Options
-require("plug") -- Plugins
-require("init_plug")
-require("keys") -- Keymaps
+require("vars")
+if vim.fn.getfsize(vim.fn.expand("%")) < 1024 * 1024 then
+    require("opts")
+    require("plug")
+    require("init_plug")
+    require("keys")
+end
